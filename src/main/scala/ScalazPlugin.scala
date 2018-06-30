63bcae737ecd3598b2bca8be6ed803f8ebd4504b
Valentin Kasas <valentin.kasas@gmail.com>
2018-07-01 00:46:51 +0200

    Add compilation configurations (#5)
    * Add compilation configurations
    
    * Add cross scala versions support

--- src/main/scala/ScalazPlugin.scala
+++ src/main/scala/ScalazPlugin.scala
@@ -1,3 +1,5 @@
+package scalaz.build
+
 import sbt._, Keys._
 
 import de.heikoseeberger.sbtheader.HeaderPlugin
@@ -8,11 +10,15 @@ object ScalazPlugin extends AutoPlugin {
 
   override def requires = HeaderPlugin
 
-  override def buildSettings = Seq(
-    organization     := "org.scalaz",
-    organizationName := "Scalaz",
-    startYear        := Some(2018),
-    licenses         += ("Apache-2.0", new URL("https://www.apache.org/licenses/LICENSE-2.0.txt"))
-  )
-
+  override def buildSettings =
+    Compilation.buildSettings ++
+    Seq(
+      organization     := "org.scalaz",
+      organizationName := "Scalaz",
+      startYear        := Some(2018),
+      licenses         += ("Apache-2.0", new URL("https://www.apache.org/licenses/LICENSE-2.0.txt"))
+    )
+  
+  override def projectSettings = 
+    Compilation.projectSettings
 }
